22adbee8078c8d928aad0b15cdac1d69e392dc91
jdmedlock <jdmedlock@gmail.com>
2023-12-30 10:14:45 -0600

    Feature: Add support for categories command
    Added a new command `categories` and completed unit testing of it.

--- src/index.js
+++ src/index.js
@@ -11,4 +11,14 @@ program.command('random')
     console.log('\nHere is a random Chuck Norris fact:\n\n', fact.value.trim())
   })
 
+  program.command('categories')
+  .description('Retrieve categories for Chuck Norris facts')
+  .action(async (str, options) => {
+    const categories = await (await fetch(process.env.API_URL.concat('categories'))).json()
+    console.log('\nHere are the categories for Chuck Norris facts:\n\n')
+    categories.forEach(category =>
+      console.log(category)
+    )
+  })
+
 program.parse()
